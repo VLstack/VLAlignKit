483250188843ae0eba659aede0358512b8742ae5
VLstack <github@vlstack.fr>
2024-10-07 11:28:40 +0200

    first commit

--- Sources/VLAlignKit/VLAlignKit.swift
+++ Sources/VLAlignKit/VLAlignKit.swift
@@ -1,2 +1,14 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import SwiftUI
+
+extension VerticalAlignment
+{
+ public struct VLMiddleImageText: AlignmentID
+ {
+  public static func defaultValue(in context: ViewDimensions) -> CGFloat
+  {
+   context[.top]
+  }
+ }
+
+ public static let vlMiddleImageText = VerticalAlignment(VLMiddleImageText.self)
+}
